290c5b4eb4f02202123694f9bf27c1532d0f716c
Mr Wayne <a4arpon@gmail.com>
2023-08-04 18:48:57 +0600

    title error fixed

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+import { Helmet } from 'react-helmet-async'
 import toast from 'react-hot-toast'
 import { Link, useNavigate } from 'react-router-dom'
 import useAuth from '../../../hooks/useAuth'
@@ -26,68 +27,73 @@ const Login = () => {
     })
   }
   return (
-    <div className="hero min-h-screen">
-      <div className="hero-content flex-col lg:flex-row w-full">
-        <div className="text-center lg:text-left lg:w-1/2">
-          <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6 text-xl">
-            To contribute your knowledge with others.
-          </p>
-        </div>
-        <div className="lg:w-1/2 card flex-shrink-0 w-full shadow-2xl bg-base-100">
-          <div className="card-body">
-            <form onSubmit={handleEmailSignIn}>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="Enter Your Email Here"
-                  className="input input-bordered"
-                  name="email"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="Enter Your Password Here"
-                  className="input input-bordered"
-                  name="password"
-                  required
-                />
-              </div>
-              <div className="mt-4">
-                <p>
-                  Don&rsquo;t have any account?{' '}
-                  <Link to="/register" className="link font-semibold">
-                    Register Now
-                  </Link>
-                </p>
-              </div>
-              <div className="form-control mt-3">
-                <button className="btn btn-neutral font-bold" type="submit">
-                  Login
+    <>
+      <Helmet>
+        <title>Login | StudyVerse</title>
+      </Helmet>
+      <div className="hero min-h-screen">
+        <div className="hero-content flex-col lg:flex-row w-full">
+          <div className="text-center lg:text-left lg:w-1/2">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6 text-xl">
+              To contribute your knowledge with others.
+            </p>
+          </div>
+          <div className="lg:w-1/2 card flex-shrink-0 w-full shadow-2xl bg-base-100">
+            <div className="card-body">
+              <form onSubmit={handleEmailSignIn}>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Enter Your Email Here"
+                    className="input input-bordered"
+                    name="email"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Enter Your Password Here"
+                    className="input input-bordered"
+                    name="password"
+                    required
+                  />
+                </div>
+                <div className="mt-4">
+                  <p>
+                    Don&rsquo;t have any account?{' '}
+                    <Link to="/register" className="link font-semibold">
+                      Register Now
+                    </Link>
+                  </p>
+                </div>
+                <div className="form-control mt-3">
+                  <button className="btn btn-neutral font-bold" type="submit">
+                    Login
+                  </button>
+                </div>
+              </form>
+              <div className="divider">OR</div>
+              <div className="mb-2">
+                <button
+                  className="btn bg-blue-500 hover:bg-blue-600 border-none w-full text-white font-bold"
+                  onClick={handleAuthWithGoogle}
+                >
+                  Continue With Google
                 </button>
               </div>
-            </form>
-            <div className="divider">OR</div>
-            <div className="mb-2">
-              <button
-                className="btn bg-blue-500 hover:bg-blue-600 border-none w-full text-white font-bold"
-                onClick={handleAuthWithGoogle}
-              >
-                Continue With Google
-              </button>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- src/pages/auth/register/Register.jsx
+++ src/pages/auth/register/Register.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+import { Helmet } from 'react-helmet-async'
 import toast from 'react-hot-toast'
 import { Link, useNavigate } from 'react-router-dom'
 import useAuth from '../../../hooks/useAuth'
@@ -39,80 +40,85 @@ const Register = () => {
     })
   }
   return (
-    <div className="hero min-h-screen">
-      <div className="hero-content flex-col lg:flex-row w-full">
-        <div className="text-center lg:text-left lg:w-1/2">
-          <h1 className="text-5xl font-bold">Register now!</h1>
-          <p className="py-6 text-xl">
-            To contribute your knowledge with others.
-          </p>
-        </div>
-        <div className="lg:w-1/2 card flex-shrink-0 w-full shadow-2xl bg-base-100">
-          <div className="card-body">
-            <form onSubmit={handleEmailSignup}>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Your Name</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="Enter Your Email Here"
-                  className="input input-bordered"
-                  name="name"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Your Email</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="Enter Your Email Here"
-                  className="input input-bordered"
-                  name="email"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Set Password</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="Enter Your Password Here"
-                  className="input input-bordered"
-                  name="password"
-                  required
-                />
-              </div>
-              <div className="mt-4">
-                <p>
-                  Already have an account?{' '}
-                  <Link to="/login" className="link font-semibold">
-                    Login Now
-                  </Link>
-                </p>
-              </div>
-              <div className="form-control mt-3">
-                <button className="btn btn-neutral font-bold" type="submit">
-                  Register
+    <>
+      <Helmet>
+        <title>Register | StudyVerse</title>
+      </Helmet>
+      <div className="hero min-h-screen">
+        <div className="hero-content flex-col lg:flex-row w-full">
+          <div className="text-center lg:text-left lg:w-1/2">
+            <h1 className="text-5xl font-bold">Register now!</h1>
+            <p className="py-6 text-xl">
+              To contribute your knowledge with others.
+            </p>
+          </div>
+          <div className="lg:w-1/2 card flex-shrink-0 w-full shadow-2xl bg-base-100">
+            <div className="card-body">
+              <form onSubmit={handleEmailSignup}>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Your Name</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Enter Your Email Here"
+                    className="input input-bordered"
+                    name="name"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Your Email</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Enter Your Email Here"
+                    className="input input-bordered"
+                    name="email"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Set Password</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Enter Your Password Here"
+                    className="input input-bordered"
+                    name="password"
+                    required
+                  />
+                </div>
+                <div className="mt-4">
+                  <p>
+                    Already have an account?{' '}
+                    <Link to="/login" className="link font-semibold">
+                      Login Now
+                    </Link>
+                  </p>
+                </div>
+                <div className="form-control mt-3">
+                  <button className="btn btn-neutral font-bold" type="submit">
+                    Register
+                  </button>
+                </div>
+              </form>
+              <div className="divider">OR</div>
+              <div className="mb-2">
+                <button
+                  className="btn bg-blue-500 hover:bg-blue-600 border-none w-full text-white font-bold"
+                  onClick={handleAuthWithGoogle}
+                >
+                  Continue With Google
                 </button>
               </div>
-            </form>
-            <div className="divider">OR</div>
-            <div className="mb-2">
-              <button
-                className="btn bg-blue-500 hover:bg-blue-600 border-none w-full text-white font-bold"
-                onClick={handleAuthWithGoogle}
-              >
-                Continue With Google
-              </button>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
